c300fe6f8644948291638cb10779bc87ffc94dee
fike <werkinehfikadu@gmail.com>
2023-02-05 15:34:31 +0200

    first automation project second version

--- DemoblazeStore/Locators/about_us_locators.py
+++
@@ -1,4 +0,0 @@
-class AboutUsLocators:
-    click_about_us_test_xpath= "/html[1]/body[1]/nav[1]/div[1]/ul[1]/li[3]"
-    select_vidio_test_xpath = "/html[1]/body[1]/div[4]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/div[1]"
-    click_close_test_xpath = "/html[1]/body[1]/div[4]/div[1]/div[1]/div[3]/button[1]"
--- DemoblazeStore/Locators/log_in_locators.py
+++
@@ -1,5 +0,0 @@
-class LogInLocators:
-    click_log_in_id = "login2"
-    enter_username_id = "loginusername"
-    enter_password_id = "loginpassword"
-    click_log_in_text_xpath = "/html[1]/body[1]/div[3]/div[1]/div[1]/div[3]/button[2]"
